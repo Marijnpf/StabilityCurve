f65924cf26728adee22af4825b2b88c361af84bc
Marijnpf <marijnpf@gmail.com>
2024-02-05 20:22:18 +0100

    made some small improvements

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from stability_curve import Vessel
 
 
 if __name__ == '__main__':
-    vessel = Vessel(Path('data/example_hull.stl'), length=160, breadth=20, draft=6, center_of_gravity=np.array([60, 0, 10]))
+    vessel = Vessel(Path('data/example_hull_quads.stl'), length=160, breadth=20, draft=6,
+                    center_of_gravity=np.array([60, 0, 10]))
     vessel.stability_curve(heeling_range=[-5, 90], increment=1)
-    vessel.animate_3d_mesh()
+    # vessel.animate_3d_mesh()
     vessel.animate_cross_section()
